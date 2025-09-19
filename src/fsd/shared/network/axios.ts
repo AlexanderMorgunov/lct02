b2582221f8797542ca2936ccdba8dc11dfdccee6
Aleksey Strizhenko <lokolex310188@gmail.com>
2025-09-19 14:05:05 +0400

    login feat

--- src/fsd/shared/network/axios.ts
+++ src/fsd/shared/network/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const $reqApi = axios.create({
-  baseURL: `https://${process.env.NEXT_PUBLIC_APP_BASE_URL}`,
+  baseURL: `http://${process.env.NEXT_PUBLIC_APP_BASE_URL}`,
   headers: {
     "Accept-Language": "ru",
     "Content-Type": "application/json",
