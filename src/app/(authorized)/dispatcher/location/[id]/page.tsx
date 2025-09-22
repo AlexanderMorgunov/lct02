4dddb2444831baa41d4c440696525a5a470075d8
Alexander Morgunov <morgunowaleks@yandex.ru>
2025-09-23 01:13:12 +0300

    fix:

--- src/app/(authorized)/dispatcher/location/[id]/page.tsx
+++ src/app/(authorized)/dispatcher/location/[id]/page.tsx
@@ -1,6 +1,5 @@
 import { LocationPage } from "@/fsd/pages/Location/LocationPage";
-import React from "react";
 
-export default function Page({ params }: { params: { id: string } }) {
+export default async function Page({ params }: { params: { id: string } }) {
   return <LocationPage id={params.id} />;
 }
